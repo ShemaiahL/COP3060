3b423cf0a75b509b44785f90667c4443d1f29e47
ShemaiahL <157056095+ShemaiahL@users.noreply.github.com>
2025-10-06 19:54:43 -0400

    Create app.js
    added variables and a function to update the status

---
+++ app.js
@@ -0,0 +1,22 @@
+"use strict"
+console.log("Welcome to PA02 Assigment");
+console.log("https://github.com/ShemaiahL/COP3060");
+
+const $ = (sel) => document.querySelector(sel);
+const on = (el, evt,fn) => el && el.addEventListener(evt,fn);
+
+function setStatus(message, type = "info") {
+    const box = $("#status");
+    if(!box) return;
+    box.textContext = message;
+}
+
+const state = {
+    myString: "Rattlers on top",
+    myNumber: 33,
+    myBool: true,
+    myArray: ["Green", "Yellow", "Orange", "Purple", "Pink"],
+    myObject: {course: "PA02", ok: true},
+    mystery: null,
+    notSet: undefined,
+};
